ac605b528ab50fb7841305cae4a79c20af30cfd3
Len Huang <32604574+lenghuang@users.noreply.github.com>
2018-10-30 20:27:17 -0400

    Add files via upload
    Beginner: Learn a bit of Python by doing a few simple exercises

---
+++ GCITaskOne.py
@@ -0,0 +1,15 @@
+# Google Code-in
+# Beginner: Learn a bit of python by doing a few simple exercises - CCExtractor Development
+# Created by eliteorigin
+
+# Using a loop, print 10 times "GCI is great"
+for x in range(9):
+    print("GCI is great")
+
+# Ask the user for his/her name and then print "Hellow (name), pleased to meet you!"
+name = raw_input("What is your name: ")
+print("Hello " + name + ", pleased to meet you!")
+
+# Print that name backwords
+nameBack = name[::-1]
+print("Did you know that your name backwards is " + nameBack)
